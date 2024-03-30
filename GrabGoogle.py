d64e5cea172784ae19ea34fb1426618c7070ae84
foofmeister <forest.summers@gmail.com>
2024-03-29 23:05:21 -0400

    second commit

--- GrabGoogle.py
+++ GrabGoogle.py
@@ -4,3 +4,18 @@ forest = """
 Test to see if this works
 """
 print(forest)
+
+
+import urllib.request
+
+url = 'https://google.com/search?q=restaurants+80501'
+
+# Perform the request
+request = urllib.request.Request(url)
+
+# Set a normal User Agent header, otherwise Google will block the request.
+#request.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36')
+raw_response = urllib.request.urlopen(request).read()
+
+# Read the repsonse as a utf-8 string
+html = raw_response.decode("utf-8")
